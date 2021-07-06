b0daf98c150de8ea5713d1128909d4b5a23ad81d
sgdg <seungdeng17@gmail.com>
2021-07-06 20:11:12 +0900

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,13 @@
+import { Route, Switch } from 'react-router-dom';
+
 export default function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Temp} />
+    </Switch>
+  );
+}
+
+function Temp() {
   return <div>hello world</div>;
 }
