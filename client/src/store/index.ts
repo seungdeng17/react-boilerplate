c787e6d0be7ea6759823c7fb2ccdcfc076bea6b7
sgdg <seungdeng17@gmail.com>
2021-07-06 19:52:36 +0900

    init

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,13 +1,15 @@
+import { combineReducers } from 'redux';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import { all } from 'redux-saga/effects';
 import commonReducer from './common';
 import commonSaga from './common/saga';
 
+const rootReducer = combineReducers({ common: commonReducer });
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: { common: commonReducer },
+  reducer: rootReducer,
   middleware: [...getDefaultMiddleware({ thunk: false }), sagaMiddleware],
   devTools: process.env.NODE_ENV === 'development',
 });
@@ -17,4 +19,5 @@ function* rootSaga() {
 }
 sagaMiddleware.run(rootSaga);
 
+export type RootState = ReturnType<typeof rootReducer>;
 export default store;
