ed8441e603c0065c39ed78ce3e3329947d3bf8a2
sgdg <seungdeng17@gmail.com>
2021-07-06 21:33:29 +0900

    init

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const db = require('./db');
 
+const app = express();
+const port = 3001;
+
+app.db = db;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use('/api', require('./routes'));
-
-const port = 3001;
 
 app.listen(port, function () {
   console.log('Express server has started on port ' + port);
