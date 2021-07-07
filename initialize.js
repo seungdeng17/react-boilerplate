e41b4a2e59400069c5b33ac0f8769945fa0e3046
sgdg <seungdeng17@gmail.com>
2021-07-07 17:05:54 +0900

    feat: initialize script

--- initialize.js
+++ initialize.js
@@ -15,35 +15,39 @@ const client_cwd = {
   cwd: path.resolve(__dirname, './client'),
 };
 
-shell.exec('npm install', root_cwd);
-shell.exec('npm install', server_cwd);
-shell.exec('npm install', client_cwd);
+(function () {
+  const { stdout } = shell.exec('yarn -v', root_cwd);
 
-shell.exec('touch .env', server_cwd);
-shell.exec('touch .env.development', client_cwd);
-shell.exec('touch .env.production', client_cwd);
+  shell.exec('yarn', root_cwd);
+  shell.exec('yarn', server_cwd);
+  shell.exec('yarn', client_cwd);
 
-{
-  const envText = `PORT=${SERVER_PORT}`;
-  fs.writeFileSync('./server/.env', envText, 'utf8');
-}
-{
-  const envText = `REACT_APP_API_HOST=http://localhost:${SERVER_PORT}`;
-  fs.writeFileSync('./client/.env.development', envText, 'utf8');
-}
-{
-  const envText = `REACT_APP_API_HOST=http://localhost:${CLIENT_PORT}`;
-  fs.writeFileSync('./client/.env.production', envText, 'utf8');
-}
-{
-  const proxy_server = fs.readFileSync('./proxy-server.js', 'utf8');
-  const updated_proxy_server = proxy_server.replace('http://localhost:3001', `http://localhost:${SERVER_PORT}`);
-  fs.writeFileSync('./proxy-server.js', updated_proxy_server, 'utf8');
-}
-{
-  const app = fs.readFileSync('./server/app.js', 'utf8');
-  const updated_app = app.replace('http://localhost:3000', `http://localhost:${CLIENT_PORT}`);
-  fs.writeFileSync('./server/app.js', updated_app, 'utf8');
-}
+  shell.exec('touch .env', server_cwd);
+  shell.exec('touch .env.development', client_cwd);
+  shell.exec('touch .env.production', client_cwd);
 
-shell.exec('npm run dev', root_cwd);
+  {
+    const envText = `PORT=${SERVER_PORT}`;
+    fs.writeFileSync('./server/.env', envText, 'utf8');
+  }
+  {
+    const envText = `REACT_APP_API_HOST=http://localhost:${SERVER_PORT}`;
+    fs.writeFileSync('./client/.env.development', envText, 'utf8');
+  }
+  {
+    const envText = `REACT_APP_API_HOST=http://localhost:${CLIENT_PORT}`;
+    fs.writeFileSync('./client/.env.production', envText, 'utf8');
+  }
+  {
+    const proxy_server = fs.readFileSync('./proxy-server.js', 'utf8');
+    const updated_proxy_server = proxy_server.replace('http://localhost:3001', `http://localhost:${SERVER_PORT}`);
+    fs.writeFileSync('./proxy-server.js', updated_proxy_server, 'utf8');
+  }
+  {
+    const app = fs.readFileSync('./server/app.js', 'utf8');
+    const updated_app = app.replace('http://localhost:3000', `http://localhost:${CLIENT_PORT}`);
+    fs.writeFileSync('./server/app.js', updated_app, 'utf8');
+  }
+
+  shell.exec('yarn dev', root_cwd);
+})();
