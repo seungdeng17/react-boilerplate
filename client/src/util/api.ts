692ffffc66a9c9c2197f955fb3a89bca4df16353
sgdg <seungdeng17@gmail.com>
2021-07-07 12:31:03 +0900

    v1.1.0

--- client/src/util/api.ts
+++ client/src/util/api.ts
@@ -3,23 +3,20 @@ import { API_HOST, ResultCode } from '@constant/fetch';
 
 export function request({ method = 'get', url, params, data }: { [key: string]: any }) {
   return axios({
-    url,
     method,
+    url,
     baseURL: API_HOST,
     params,
     data,
     withCredentials: true,
   }).then((response) => {
-    const { resultCode, resultMessage, totalCount } = response.data;
+    const { resultCode } = response.data;
     if (resultCode < 0) {
       console.warn('http error!');
     }
     return {
       isSuccess: resultCode === ResultCode.Success,
-      data: response.data.data,
       resultCode,
-      resultMessage,
-      totalCount,
     };
   });
 }
