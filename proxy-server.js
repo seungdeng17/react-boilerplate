3d54f21efb858d1c2df92049eb4e3242eb5b2143
sgdg <seungdeng17@gmail.com>
2021-07-07 14:21:37 +0900

    init

--- proxy-server.js
+++ proxy-server.js
@@ -5,9 +5,8 @@ const app = express();
 app.use('/api', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 
 app.use(express.static('public'));
-app.use('/favicon.png', express.static(__dirname + '/dist/favicon.png'));
+app.use('/favicon.ico', express.static(__dirname + '/dist/favicon.ico'));
 app.use('/static', express.static(__dirname + '/dist/static'));
-app.use('/index.js', express.static(__dirname + '/dist/index.js'));
 app.get('/*', (_, res) => {
   res.status(200).sendFile(__dirname + '/dist/index.html');
 });
