1aa4bb937c2fb15dbfca950e93b696025fe56f5c
sgdg <seungdeng17@gmail.com>
2021-07-07 13:33:52 +0900

    modify: webpack common

--- client/webpack/common.js
+++ client/webpack/common.js
@@ -33,7 +33,7 @@ module.exports = {
         test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'url-loader',
         options: {
-          name: '[hash].[ext]',
+          name: 'static/[hash].[ext]',
           limit: 10000,
         },
       },
