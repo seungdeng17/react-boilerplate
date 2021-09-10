d4a4d4ba84d877a39ce1c6a5e0b59e9660eebe76
이승택 <seungdeng17@gmail.com>
2021-09-10 10:56:50 +0900

    modify: 파일 수정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,10 @@
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
