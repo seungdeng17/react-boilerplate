9e07b895f547f9433582e63e10ac180c25454b13
이승택 <seungdeng17@gmail.com>
2022-01-15 14:29:49 +0900

    modify: ts, sb config

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -8,14 +8,14 @@ module.exports = {
   webpackFinal: async (config, { configType }) => {
     config.resolve.alias = {
       '@': path.resolve(__dirname, '../', 'src/'),
-      '@asset': path.resolve(__dirname, '../', 'src/asset/'),
+      '@assets': path.resolve(__dirname, '../', 'src/assets/'),
       '@components': path.resolve(__dirname, '../', 'src/components/'),
-      '@constant': path.resolve(__dirname, '../', 'src/constant/'),
-      '@hook': path.resolve(__dirname, '../', 'src/hook/'),
-      '@page': path.resolve(__dirname, '../', 'src/page/'),
-      '@store': path.resolve(__dirname, '../', 'src/store/'),
-      '@style': path.resolve(__dirname, '../', 'src/style/'),
-      '@util': path.resolve(__dirname, '../', 'src/util/'),
+      '@constants': path.resolve(__dirname, '../', 'src/constants/'),
+      '@hooks': path.resolve(__dirname, '../', 'src/hooks/'),
+      '@pages': path.resolve(__dirname, '../', 'src/pages/'),
+      '@state': path.resolve(__dirname, '../', 'src/state/'),
+      '@styles': path.resolve(__dirname, '../', 'src/styles/'),
+      '@utils': path.resolve(__dirname, '../', 'src/utils/'),
     };
 
     return config;
